37402f22d186dd7c3038978f68b14a859fd08647
ozivic <ozivic@descartes.com>
2020-04-16 13:50:46 +0200

    convert images

---
+++ ImageFormatConverter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace ImageFormatConverter
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Load the image.
+            Image image1 = Image.FromFile("test-image.png");
+
+            // Save the image in JPEG format.
+            image1.Save("test-image.jpg", ImageFormat.Jpeg);
+
+            // Save the image in GIF format.
+            image1.Save("test-image.gif", ImageFormat.Gif);
+        }
+    }
+}
